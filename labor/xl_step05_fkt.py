71aa504fa0d085c92e7d509aef389df6db80e462
JaRe <jrentenatus@outlook.de>
2025-12-17 18:43:48 +0100

    filtering

--- labor/xl_step05_fkt.py
+++ labor/xl_step05_fkt.py
@@ -29,6 +29,13 @@ class Step05(Runnable):
 
         # Lade dein bestehendes DataFrame
         all_df = load_dataframe("assets/output/xl_step03_code")
+        sign_dict = {}
+        for idx, row in all_df.iterrows():
+            meaning = row.meaning
+            if meaning.startswith("++"):
+                continue
+            signatur = row.signatur
+            if pd.notna(signatur): sign_dict[meaning] = signatur
 
         print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
@@ -37,9 +44,17 @@ class Step05(Runnable):
         print(f"Gefundene Formeln: {len(formulas)}")
         # Beispielausgabe
         for k, v in list(formulas.items()):
-            print(k, "->", v)
 
-
+            fkt_code = v[4] # Index 4 enthält den VBA-Code der Funktion
+            used_names=[]
+            used_meanings=[]
+            for nr in named_ranges.keys():
+                if nr in str(fkt_code):  # robust: str() falls None
+                    used_names.append(nr)
+            for mean in sign_dict.keys():
+                if mean in str(fkt_code):
+                    used_meanings.append(mean)
+            print(k, "->", v, "  used names:", used_names, " used meanings:", used_meanings)
 
         print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
